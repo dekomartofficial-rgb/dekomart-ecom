6d5a7b86c34384f3680bf3fc5401ca522321e75c
Mohammed Sinan C <sinanruzz9@gmail.com>
2025-02-19 02:15:51 +0530

    prime ng and boostrap added

--- Cartify/src/app/app.config.ts
+++ Cartify/src/app/app.config.ts
@@ -1,6 +1,9 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeng/themes/aura';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
@@ -8,6 +11,8 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideHttpClient(
       withFetch() // Optional: Use Fetch API instead of XMLHttpRequest
-    )
+    ),
+    provideAnimationsAsync(),
+    providePrimeNG({ theme: { preset: Aura } })
   ]
 };
