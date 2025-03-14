789525976862fac3cb99f8fb1e3076bbd76e4452
zaid-thabsheer <97686474+zaid-thabsheer@users.noreply.github.com>
2025-03-14 23:48:01 +0530

    home-updated

---
+++ Cartify/src/app/home/section/banner-home/banner-home.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common'; 
+
+@Component({
+  selector: 'app-banner-home',
+  templateUrl: './banner-home.component.html',
+  standalone: true,
+  imports: [CommonModule], 
+  styleUrls: ['./banner-home.component.css'],
+  
+})
+export class BannerHomeComponent {
+  banners = [
+    { image: 'images/banner1.jpg', title: 'Expressive ART PAINTINGS', subtitle: '20% OFF ON ENTIRE RANGE' },
+    { image: 'images/banner2.jpg', title: 'Modern Home Decor', subtitle: 'Best Designs for Your Home' }
+  ];
+}
