57f31e10f497df59ff35b2828e9400c2f6345a91
zaid-thabsheer <97686474+zaid-thabsheer@users.noreply.github.com>
2025-03-29 16:03:48 +0530

    addtocart component

--- Cartify/src/app/home/section/nav-home/nav-home.component.ts
+++ Cartify/src/app/home/section/nav-home/nav-home.component.ts
@@ -2,17 +2,23 @@ import { Component, Input } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { HttpClientService } from '@/app/provider/services/http-client.service';
 import { Router } from '@angular/router';
+import { AddToCartComponent } from "../add-to-cart/add-to-cart.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-nav-home',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, AddToCartComponent,CommonModule],
   templateUrl: './nav-home.component.html',
   styleUrl: './nav-home.component.css'
 })
 export class NavHomeComponent {
   @Input() wishlistCount: number = 0;
-  @Input () cartCount: number =0;
+  @Input () cartItem: any= [];
+  
+
+  showCart: boolean = false; 
+
   constructor(private http: HttpClientService, private router: Router) { }
 
   onLogin() {
@@ -27,4 +33,15 @@ export class NavHomeComponent {
       return this.router.navigateByUrl('/login')
     }
   }
+
+ 
+  toggleCart() {
+    this.showCart = !this.showCart;  
+  }
+
+  CloseCart() {
+    console.log("Cart closed!");
+    this.showCart = false;  // Hide the cart
+  }
+
 }
