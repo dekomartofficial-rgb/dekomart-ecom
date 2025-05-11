e594113fbe882adda4247f1e32ca7babc89c5821
Mohammed Sinan C <sinanruzz9@gmail.com>
2025-05-11 23:05:15 +0530

    commit

--- CartifyWebApi/index.js
+++ CartifyWebApi/index.js
@@ -28,7 +28,7 @@ app.use("/user", userRoutes);
 app.use("/admin", adminRoutes);
 
 app.get('/', (req, res) => {
-  res.send('API is running...');
+  res.send('API is running...connected to database');
 });
 
 
