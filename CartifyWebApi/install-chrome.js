9e6f894dedd9da03b09917d047e12b522737d467
Mohammed Sinan C <111516035+MohammedSinanC@users.noreply.github.com>
2025-07-03 00:34:46 +0530

    chnags

--- CartifyWebApi/install-chrome.js
+++ CartifyWebApi/install-chrome.js
@@ -1,9 +1,11 @@
 // CartifyWebApi/install-chrome.js
 
-const puppeteer = require('puppeteer');
+const { install } = require('puppeteer/browsers');
 
 (async () => {
-  await puppeteer.install({
-    browser: 'chrome'
+  await install({
+    browser: 'chrome',
+    buildId: '138.0.7204.92' // specify exact version needed
   });
 })();
+s
